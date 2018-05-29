c8029c67ab8244c330d654ec0877c8099e2cd28b
Aditya Mehra <Aix.m@outlook.com>
2018-05-29 21:43:18 +0930

    Revert "Update for 18.02 branch"
    This reverts commit 2d1737d9959cb2c8c9eeff926a526a07ac8b4aa9.

--- __init__.py
+++ __init__.py
@@ -1,47 +1,59 @@
 import sys
-import pytesseract
-import enchant
-import re
-from nltk import word_tokenize
+import dbus
+import glib
 from traceback import print_exc
 from os.path import dirname
 from adapt.intent import IntentBuilder
-from mycroft.skills.core import MycroftSkill, intent_handler
+from mycroft.skills.core import MycroftSkill
 from mycroft.util.log import getLogger
-from PIL import Image, ImageEnhance, ImageFilter
- 
+from clarifai.rest import ClarifaiApp
+from clarifai.rest import Image as ClImage 
+
 __author__ = 'aix'
 
 LOGGER = getLogger(__name__)
 
-class ImgOcrPlasmaDesktopSkill(MycroftSkill):
+class ImgRecogPlasmaDesktopSkill(MycroftSkill):
 
     # The constructor of the skill, which calls MycroftSkill's constructor
     def __init__(self):
-        super(ImgOcrPlasmaDesktopSkill, self).__init__(name="ImgOcrPlasmaDesktopSkill")
+        super(ImgRecogPlasmaDesktopSkill, self).__init__(name="ImgRecogPlasmaDesktopSkill")
         
-    @intent_handler(IntentBuilder("ImgOcrKeywordIntent").require("ImgOcrKeyword").build())
-    def handle_imgocr_general_plasma_skill_intent(self, message):
+    # This method loads the files needed for the skill's functioning, and
+    # creates and registers each intent that the skill uses
+    def initialize(self):
+        self.load_data_files(dirname(__file__))
+
+        imgrecog_general_plasma_skill_intent = IntentBuilder("ImgRecogKeywordIntent").\
+            require("ImgRecogKeyword").build()
+        self.register_intent(imgrecog_general_plasma_skill_intent, self.handle_imgrecog_general_plasma_skill_intent)
+        
+    def handle_imgrecog_general_plasma_skill_intent(self, message):
         utterance = message.data.get('utterance')
-        utterance = utterance.replace(message.data.get('ImgOcrKeyword'), '')
+        utterance = utterance.replace(
+                message.data.get('ImgRecogKeyword'), '')
         searchString = utterance
         imgRurl = searchString.replace(searchString[:8], '')
-        im = Image.open(imgRurl)
-        enhancer = ImageEnhance.Contrast(im)
-        im = im.filter(ImageFilter.SHARPEN)
-        im = im.filter(ImageFilter.EDGE_ENHANCE)
-        im.save('/tmp/image.jpg')
-        getOCR = Image.open('/tmp/image.jpg')
-        ocrtext = pytesseract.image_to_string(getOCR)
-        self.speak(ocrtext)
-    
-    def extract_words(self, s):
-        return [re.sub('^[{0}]+|[{0}]+$'.format(string.punctuation), '', w) for w in s.split()]
+        
+        app = ClarifaiApp("gg_0vhbsOoZGq6N85i3yuKKcHj5JvTLrX62KeeyD", "YOKgEUJc6SgikEREgPE3iSkRHd11tY4w2alSghot")
+        model = app.models.get('general-v1.3')
+        image = ClImage(file_obj=open(imgRurl, 'rb'))
+        a = model.predict([image])
+        
+        b = ''
+        for z in a['outputs']:
+            for y in z['data']['concepts']:
+                if not b :
+                    b =  y['name']
+                else:
+                    b = b + ',' + y['name']
 
+        self.speak_dialog("imgresult.general", data={'GeneralResult': b})
+        
     def stop(self):
         pass
 
 # The "create_skill()" method is used to create an instance of the skill.
 # Note that it's outside the class itself.
 def create_skill():
-    return ImgOcrPlasmaDesktopSkill()
+    return ImgRecogPlasmaDesktopSkill()
